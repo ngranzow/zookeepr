658c1e676af04690be235cd136ac9c151ea6b81a
ngranzow <nate.granzow@gmail.com>
2022-03-02 00:05:29 -0600

    handle requests for a specific animal

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals.json')
@@ -30,12 +31,28 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+app.use(morgan('dev'));
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
